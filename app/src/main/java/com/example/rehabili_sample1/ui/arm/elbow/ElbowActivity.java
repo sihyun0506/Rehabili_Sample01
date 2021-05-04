9b024cf429cad81163bf9812045a789632195688
정승현 <vergissmeinnicht31@gmail.com>
2021-05-05 03:28:52 +0900

    ElbowActivity 추가작성

--- app/src/main/java/com/example/rehabili_sample1/ui/arm/elbow/ElbowActivity.java
+++ app/src/main/java/com/example/rehabili_sample1/ui/arm/elbow/ElbowActivity.java
@@ -2,19 +2,22 @@ package com.example.rehabili_sample1.ui.arm.elbow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import com.example.rehabili_sample1.R;
+import com.example.rehabili_sample1.ui.arm.ArmActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ElbowActivity extends AppCompatActivity {
     private WebView webView;
     private String url = "https://www.youtube.com/embed/DrKY37Tz41M";
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,16 @@ public class ElbowActivity extends AppCompatActivity {
         webView.loadUrl(url);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClientClass());
+        FloatingActionButton elbowNext = (FloatingActionButton)findViewById(R.id.nextElbow);
+
+
+        elbowNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ElbowActivity.this, ElbowSet.class);
+                startActivity(intent);
+            }
+        });
 
     }
     @Override
--- app/src/main/java/com/example/rehabili_sample1/ui/arm/elbow/ElbowSet.java
+++ app/src/main/java/com/example/rehabili_sample1/ui/arm/elbow/ElbowSet.java
@@ -1,66 +1,71 @@
 package com.example.rehabili_sample1.ui.arm.elbow;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.rehabili_sample1.R;
+import com.example.rehabili_sample1.ui.arm.ArmActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ElbowSet#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ElbowSet extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ElbowSet() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ElbowSet.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ElbowSet newInstance(String param1, String param2) {
-        ElbowSet fragment = new ElbowSet();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class ElbowSet extends AppCompatActivity {
+    Button start;
+    EditText editText;
+    Spinner spinner;
+    public String goalNumber;
+    public String level;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.activity_elbow_set);
+        start = (Button)findViewById(R.id.buttonStart);
+        editText = (EditText)findViewById(R.id.goalNumber);
+        spinner = (Spinner)findViewById(R.id.level);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                level = parent.getItemAtPosition(position).toString();
+            }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_elbow_set, container, false);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goalNumber = editText.getText().toString();
+
+                if (goalNumber.equals("") ||Integer.parseInt(goalNumber) == 0){
+                    Toast.makeText(ElbowSet.this, "올바른 값을 입력하세요" , android.widget.Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (Integer.parseInt(goalNumber) > 50){
+                    Toast.makeText(ElbowSet.this, "50회 미만의 값을 입력하세요" , android.widget.Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else {
+                    Intent intent = new Intent(ElbowSet.this, ElbowCounting.class);
+                    intent.putExtra("goalNumber",goalNumber);
+                    intent.putExtra("level",level);
+                    startActivity(intent);
+
+                }
+            }
+        });
+
+
+
+
+
     }
 }
