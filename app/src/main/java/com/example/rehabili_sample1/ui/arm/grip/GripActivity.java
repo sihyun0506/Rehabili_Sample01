3b7ec3a7ca412819db0a4955d40063979ca4095a
sihyun0506 <sihyun0506@gmail.com>
2021-05-13 16:43:40 +0900

    오른손 왼손을 추가하기 위한 activity, xml 추가(elbow, wrist(기능 미작성)) 새로운 기능 GripActivity추가를 위한 activity, xml 생성(기능 미작성) grip 이미지 수정, 오른손 grip 이미지 추가
    ======예정사항======
    set수정(right left radio button으로 추가)
    기능 미작성 부분 기능 추가
    WristCountingRight를 위한 이미지 추가

---
+++ app/src/main/java/com/example/rehabili_sample1/ui/arm/grip/GripActivity.java
@@ -0,0 +1,16 @@
+package com.example.rehabili_sample1.ui.arm.grip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rehabili_sample1.R;
+
+public class GripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grip);
+    }
+}
---
+++ app/src/main/java/com/example/rehabili_sample1/ui/arm/wrist/WristCountingRight.java
@@ -0,0 +1,16 @@
+package com.example.rehabili_sample1.ui.arm.wrist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rehabili_sample1.R;
+
+public class WristCountingRight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrist_counting_right);
+    }
+}
