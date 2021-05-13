b564c31a7192cd8675d00c4fdcdb717d522c97ba
sihyun0506 <sihyun0506@gmail.com>
2021-05-13 20:08:04 +0900

    기능 미작성 부분(grip) 기능 일부 추가 기능 설명 일부 추가
    ======예정사항======
    기능 미작성 부분(grip) 기능 추가
    
    앱 컨셉 변경(재활운동 어플 -> 팔 재활운동 어플)
    1. (완료)
    2. 메인화면 변경 (목, 어깨, 허리, 다리 삭제, ArmMain화면을 메인 화면으로 변경)
    
    기능 설명 + 영상 첨부
    
    UserEdit액티비티의 EditText hint 수정
    
    xml 파일들의 constraint 설정 점검.
    
    ArmMain 화면에 그림 터치 추가,
    
    액션바이동버튼&기능(사용자 정보, 메인으로 이동, 재활기록 확인, 앱정보) 모두 삭제

--- app/src/main/java/com/example/rehabili_sample1/ui/arm/grip/GripActivity.java
+++ app/src/main/java/com/example/rehabili_sample1/ui/arm/grip/GripActivity.java
@@ -2,15 +2,66 @@ package com.example.rehabili_sample1.ui.arm.grip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.example.rehabili_sample1.R;
+import com.example.rehabili_sample1.ui.Set;
+import com.example.rehabili_sample1.ui.arm.wrist.WristActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GripActivity extends AppCompatActivity {
+    private TextView exerciseType;
+    private String type;
+    private WebView webView;
+    private String url = "https://www.youtube.com/embed/DrKY37Tz41M";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grip);
+
+        exerciseType = (TextView) findViewById(R.id.gripText);
+        type = exerciseType.getText().toString();
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new GripActivity.WebViewClientClass());
+        FloatingActionButton gripNext = (FloatingActionButton) findViewById(R.id.nextGrip);
+        gripNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // type 값을 가지고 다음 액티비티 Set으로 이동
+                Intent intent = new Intent(GripActivity.this, Set.class);
+                intent.putExtra("type", type);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {//현재페이지의 URL을 읽음
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
