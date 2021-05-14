73837ff2f742009e2831d0622d893c09f9f6e443
정승현 <vergissmeinnicht31@gmail.com>
2021-05-14 23:55:37 +0900

    TTS 설정중 2

--- app/src/main/java/com/example/rehabili_sample1/ui/arm/elbow/ElbowActivity.java
+++ app/src/main/java/com/example/rehabili_sample1/ui/arm/elbow/ElbowActivity.java
@@ -15,7 +15,10 @@ import android.widget.TextView;
 
 import com.example.rehabili_sample1.R;
 import com.example.rehabili_sample1.ui.Set;
+import com.example.rehabili_sample1.ui.TTSReader;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
 
 import java.util.Locale;
 
@@ -24,9 +27,8 @@ public class ElbowActivity extends AppCompatActivity {
     private String type;
     private WebView webView;
     private String url = "https://www.youtube.com/embed/iA5K3X29lrA";
-    private TextToSpeech textToSpeech;
     String text;
-    int sw = 0;
+    TTSReader ttsReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,39 +44,12 @@ public class ElbowActivity extends AppCompatActivity {
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClientClass());
 
-        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    //사용할 언어를 설정
-                    Locale systemLocale = getResources().getConfiguration().locale;
-                    int result = textToSpeech.setLanguage(systemLocale);
-
-                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-
-                    } else {
-                        //음성 톤
-                        textToSpeech.setPitch(1);
-                        //읽는 속도
-                        textToSpeech.setSpeechRate(1);
-
-                    }
-                }
-            }
-        });
         TextView textView = (TextView)findViewById(R.id.elbow_explain);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sw==0) {
-                    Speech();
-                    sw=1;
-                }else {
-                    textToSpeech.stop();
-                    sw=0;
-                }
-            }
-        });
+        text = getString(R.string.elbow_text);
+        ttsReader = new TTSReader();
+        Locale systemLocale = getResources().getConfiguration().locale;
+
+        ttsReader.setTTSReader(this,textView,text,systemLocale);
 
         FloatingActionButton elbowNext = (FloatingActionButton)findViewById(R.id.nextElbow);
         elbowNext.setOnClickListener(new View.OnClickListener() {
@@ -89,30 +64,11 @@ public class ElbowActivity extends AppCompatActivity {
 
     }
 
-
-    private void Speech() {
-        // QUEUE_FLUSH: Queue 값을 초기화한 후 값을 넣는다.
-        // QUEUE_ADD: 현재 Queue에 값을 추가하는 옵션이다.
-        // API 21
-        text = getString(R.string.elbow_text);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-
-            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-            // API 20
-        else
-            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-
-    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // TTS 객체가 남아있다면 실행을 중지하고 메모리에서 제거한다.
-        if(textToSpeech != null){
-            textToSpeech.stop();
-            textToSpeech.shutdown();
-            textToSpeech = null;
-        }
+        ttsReader.ttsRemove();
     }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
